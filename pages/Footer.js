733ba3ee1b5107f2e5894acebbe5b9d6bbb995fa
Josh <52718132+JoshuaGutie@users.noreply.github.com>
2022-08-19 00:24:00 -0500

    deploy error fix

--- pages/Footer.js
+++ pages/Footer.js
@@ -4,7 +4,7 @@ import ministries from "../public/images/ministries.png"
 function Footer() {
     return ( 
         <div className="footerContainer">
-            <div className="footerLogo"><Image src={ministries} alt="logo/></div>
+            <div className="footerLogo"><Image src={ministries} alt="logo"/></div>
         </div>
      );
 }
